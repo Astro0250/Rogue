5e372369ea9c10a63364058207857352c44f8a89
sidet <sidet@Locke.lan>
2022-11-22 19:13:52 -0500

    Main Line

--- Rogue/src/main/Main.java
+++ Rogue/src/main/Main.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("balls");
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
