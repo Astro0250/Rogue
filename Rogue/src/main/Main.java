d81de2a222b802d3df3e7760e85599bf86b363c4
furio <furio@Christian-Alienware-DESKTOP>
2022-12-13 16:56:41 -0500

    REMOVES REDUNDANT COMMENT

--- Rogue/src/main/Main.java
+++ Rogue/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 
 public class Main {
-	// I AM CHRISSY
+	
 	public static void main(String[] args) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
