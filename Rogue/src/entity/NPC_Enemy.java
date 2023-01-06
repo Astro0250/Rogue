a00cd3dc1b5c9d28049a3f2d4f1e9f8a54b33080
afaynin <118326422+afaynin@users.noreply.github.com>
2023-01-05 20:50:53 -0500

    Fixed minor visual bug

--- Rogue/src/entity/NPC_Enemy.java
+++ Rogue/src/entity/NPC_Enemy.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Random;
+import java.lang.Math;
 
 import javax.imageio.ImageIO;
 
@@ -46,10 +47,18 @@ public class NPC_Enemy extends Entity{
 			
 //			Random random = new Random();
 //			int i = random.nextInt(100)+1;
+			//buffer can potentially be raised/lowered
+			if (Math.abs(gp.player.worldY  - worldY) <= 2) {
+				worldY = gp.player.worldY;
+			}
 			if (gp.player.worldY < worldY) {
 				direction = "up";
 			}else if(gp.player.worldY > worldY){
 				direction = "down";
+			}
+
+			if (Math.abs(gp.player.worldX  - worldX) <= 2) {
+				worldX = gp.player.worldX;
 			}
 			if(gp.player.worldX < worldX) {
 				direction = "left";
@@ -66,7 +75,7 @@ public class NPC_Enemy extends Entity{
 					direction = "down right";
 				}
 			}
-			
+		
 			
 //			actionLock = 0;
 //		}
