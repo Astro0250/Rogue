f963feac7dcb028794c074e5678eb9e95ae5ef98
sidet <sidet@Locke.lan>
2023-01-07 15:31:32 -0500

    AGHAGHAGHGA(implements knockback kinda)

--- Rogue/src/entity/Entity.java
+++ Rogue/src/entity/Entity.java
@@ -13,6 +13,7 @@ public class Entity {
 	public int worldY;	
 	public int speed;
 	public int health = 100;
+	public int knockback = 50;;
 	public BufferedImage up1, up2, down1, down2, left1, left2, left3, left4, right1, right2, right3, right4;
 	public BufferedImage atk1;
 	public String direction;
